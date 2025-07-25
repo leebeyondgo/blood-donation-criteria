b8d786861c7ee7e46ed05473fdef051b75342fb1
이정원 <won31080@gmail.com>
2025-07-25 16:59:26 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -19,6 +19,8 @@ function App() {
     const mm = String(date.getMonth() + 1).padStart(2, '0');
     const dd = String(date.getDate()).padStart(2, '0');
     return `${yyyy}년${mm}월${dd}일`;
+  }
+  
   const [theme, setTheme] = useState(() =>
     localStorage.getItem('theme') || 'light'
   );
