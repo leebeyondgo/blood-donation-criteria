74265b08ccdf2b0831801423643580f8393fa808
leebeyondgo <asomegarden@gmail.com>
2025-07-25 16:41:28 +0900

    test: add search functionality test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,12 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
-test('renders search input', () => {
+test('검색어 입력 시 해당 제한 조건이 표시된다', async () => {
   render(<App />);
-  const inputElement = screen.getByPlaceholderText(/검색어를 입력하세요/i);
-  expect(inputElement).toBeInTheDocument();
+  const input = screen.getByPlaceholderText(/검색어를 입력하세요/i);
+  await userEvent.type(input, 'HCV');
+  expect(
+    screen.getByText(/감염 이력 있는 경우 영구 헌혈 금지/i)
+  ).toBeInTheDocument();
 });
