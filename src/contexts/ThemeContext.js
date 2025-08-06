f8f731a61467738456cbda83fcbadb51b9680f56
이정원 <won31080@gmail.com>
2025-08-06 11:30:15 +0900

    Update ThemeContext.js

--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -36,7 +36,7 @@ export const ThemeProvider = ({ children }) => {
   };
 
   return (
-    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+    <ThemeContext.Provider value={{ theme, toggleTheme }}> {/* 이 부분을 수정했어요 */}
       {children}
     </ThemeContext.Provider>
   );
